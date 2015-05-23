5b55be66293538c61a5940c13e75be4a14533ded
rosmargie <rosmargie@github.com>
2015-05-23 00:38:43 -0500

    User modificados

--- src/Fisi/SigriBundle/Controller/TicketController.php
+++ src/Fisi/SigriBundle/Controller/TicketController.php
@@ -4,7 +4,7 @@ namespace Fisi\SigriBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class TicketController extends Controller
 {
     public function indexAction()
     {
---
+++ src/Fisi/SigriBundle/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Fisi\SigriBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{     
+    public function ConsultarSolicitateAction()
+    {
+        return $this->render('FisiSigriBundle:solicitante:consultar.html.twig');
+    }
+      public function SolicitarSolicitanteAction()
+    {
+        return $this->render('FisiSigriBundle:solicitante:solicitar.html.twig');
+    }
+    
+   public function MostrarBEPAction()
+    {
+        return $this->render('FisiSigriBundle:personal:bandejaEntrPersonal.html.twig');
+    }
+    
+    public function MostrarBEGAction()
+    {
+        return $this->render('FisiSigriBundle:gestor:bandejaEntrGestor.html.twig');
+    }
+    
+   
+    
+    
+    
+   
+}
